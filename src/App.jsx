d6a2875c28f57ee860dedb31413d630f6815883d
Anne <anne@mediacollege.dk>
2024-07-10 09:25:25 +0200

    Add a Contact-page
    Closes #7

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import Navigation from "./components/navigation/Navigation";
 import { useRoutes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   const routes = useRoutes([
     { path: "/", element: <Home /> },
     { path: "/about", element: <About /> },
-    { path: "/contact", element: <h1>Contact</h1> },
+    { path: "/contact", element: <Contact /> },
     { path: "*", element: <h1>Page not found</h1> },
   ]);
   return (
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <article>
+      <h1>Contact</h1>
+    </article>
+  );
+};
+
+export default Contact;
