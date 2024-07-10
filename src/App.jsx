4c0d1c1848410cda41c439496771cd89037f7a4b
Anne <anne@mediacollege.dk>
2024-07-10 09:07:40 +0200

    Add an About-page
    Fixes #5

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import Footer from "./components/footer/Footer";
 import Navigation from "./components/navigation/Navigation";
 import { useRoutes } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   const routes = useRoutes([
     { path: "/", element: <Home /> },
-    { path: "/about", element: <h1>About</h1> },
+    { path: "/about", element: <About /> },
     { path: "/contact", element: <h1>Contact</h1> },
     { path: "*", element: <h1>Page not found</h1> },
   ]);
