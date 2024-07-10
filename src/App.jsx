8cbbcb3cc5305c3c6d7c4a98c5f99b9ad96dcc7b
Anne <anne@mediacollege.dk>
2024-07-10 08:01:46 +0200

    Add global footer
    Closes #3

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import "./App.css";
+import Footer from "./components/footer/Footer";
 import Navigation from "./components/navigation/Navigation";
 import { useRoutes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   const routes = useRoutes([
-    { path: "/", element: <h1>Home</h1> },
+    { path: "/", element: <Home /> },
     { path: "/about", element: <h1>About</h1> },
     { path: "/contact", element: <h1>Contact</h1> },
     { path: "*", element: <h1>Page not found</h1> },
   ]);
   return (
-    <>
+    <div className='app'>
       <Navigation />
-      {routes}
-    </>
+      <div className='content'>{routes}</div>
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <article>
+      <h1>Welcome to my blog!</h1>
+    </article>
+  );
+};
+
+export default Home;
